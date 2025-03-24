66569fb5401caa3176709724a4587a0aff44d76e
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-03-24 15:24:39 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 4688 ms, Memory: 297000 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -3,27 +3,40 @@ sys.setrecursionlimit(10000000)
 
 input = sys.stdin.readline
 
-
-n  = int(input())
+n = int(input())
 
 moi = lambda: map(int, input().split())
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 
-for _ in range(n-1):
-    a,b = moi()
+for _ in range(n - 1):
+    a, b = moi()
     graph[a].append(b)
     graph[b].append(a)
 
-visit = [0] * (n+1)
-dp = [[0,1] for _ in range(n+1)]
+dp = [[0, 1] for _ in range(n + 1)]
+stack = []
+parent = [-1] * (n + 1)  # 부모 노드를 저장
 
-def dfs(x):
-    visit[x] = 1
-    for next in graph[x]:
-        if not visit[next]:
-            dfs(next)
-            dp[x][0]+= dp[next][1]
-            dp[x][1]+= min(dp[next])
+# 스택을 이용한 DFS
+def dfs_stack(root):
+    stack.append(root)
+    order = []  # 처리 순서를 기록
 
-dfs(1)
+    while stack:
+        node = stack.pop()
+        order.append(node)
+
+        for neighbor in graph[node]:
+            if neighbor != parent[node]:  # 부모 노드가 아니면 자식으로 탐색
+                parent[neighbor] = node
+                stack.append(neighbor)
+
+    # 역순으로 dp 값을 계산
+    for node in reversed(order):
+        for child in graph[node]:
+            if child != parent[node]:  # 자식 노드만 고려
+                dp[node][0] += dp[child][1]  # 현재 노드가 얼리 어답터가 아닐 경우
+                dp[node][1] += min(dp[child])  # 현재 노드가 얼리 어답터일 경우
+
+dfs_stack(1)
 print(min(dp[1]))
