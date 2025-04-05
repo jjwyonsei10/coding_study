862370921e5c054861f76b98094ceceb45f4bfb3
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-05 12:48:20 +0900

    [Silver I] Title: 1로 만들기 2, Time: 128 ms, Memory: 126368 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,20 @@
+import sys
+n = int(input())
+dp = [0] * (n+1)
+parent  = [0] * (n+1)
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 1
+    parent[i] = i-1
+    if i % 2 == 0 and dp[i//2] + 1 < dp[i]:
+        dp[i]  = dp[i//2] + 1
+        parent[i] = i//2
+    if i % 3 == 0 and dp[i//3] + 1 < dp[i]:
+        dp[i]  = dp[i//3] + 1
+        parent[i] = i//3
+print(dp[n])
+cur = parent[n]
+res = [n]
+while cur!=0:
+    res.append(cur)
+    cur = parent[cur]
+print(*res)
