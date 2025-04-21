b40e3d144b984a76bc517d30d154ccecba2ead45
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-21 15:56:03 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 118576 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,7 @@
+s = int(input())
+dp = [0] * int(1e6+1)
+for i in range(2, s+1):
+    dp[i] = dp[i-1] +1
+    if i%2==0:dp[i] = min(dp[i], dp[i//2] + 1)
+    if i%3==0:dp[i] = min(dp[i], dp[i//3] + 1)
+print(dp[s])
