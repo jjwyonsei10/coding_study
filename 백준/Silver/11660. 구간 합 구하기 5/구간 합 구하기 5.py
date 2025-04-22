f3979641aee23d203668cb619bf55d400a137511
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-22 22:30:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 288 ms, Memory: 129500 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+moi = lambda: map(int, input().split())
+
+n,m = moi()
+arr = [list(moi()) for _ in range(n)]
+
+dp = [[0] * (n+1) for _ in range(n+1)]
+
+
+for i in range(1, n+1):
+    for j in range(1, n+1):dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i-1][j-1]
+
+for _ in range(m):
+    x1,y1,x2,y2 = moi()
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
