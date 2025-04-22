0ea50081151a53450353c7697f4fceee01db2440
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-22 16:14:49 +0900

    [Gold III] Title: 나머지 합, Time: 412 ms, Memory: 256592 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -6,20 +6,13 @@ input = sys.stdin.readline
 moi = lambda: map(int, input().split())
 n, m = moi()
 arr = list(moi())
-
-# 누적합의 나머지를 저장할 리스트
-dp = [0]
-for i in range(n):
-    dp.append((dp[-1] + arr[i]) % m)
-
-# 나머지 등장 횟수 카운트
 my_dict = defaultdict(int)
-for d in dp:
-    my_dict[d] += 1
-
-# 조합 계산: 같은 나머지가 v개 있으면 vC2 = v * (v - 1) // 2
 res = 0
-for v in my_dict.values():
-    res += v * (v - 1) // 2
-
+cur_sum = 0
+my_dict[0] = 1
+for num in arr:
+    cur_sum+=num
+    mod = cur_sum%m
+    res+= my_dict[mod]
+    my_dict[mod]+=1
 print(res)
