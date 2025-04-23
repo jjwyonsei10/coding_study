30dc2caa192721e99f4a7c42cabf7741ff0fbb80
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-23 15:48:13 +0900

    [Silver I] Title: 곱셈, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,4 +1,12 @@
 import sys
 input = sys.stdin.readline
 n,m,c = map(int, input().split())
-print(pow(n,m,c))
+def cal(a,b,c):
+    if b == 1:
+        return a%c
+    tmp = cal(a, b//2, c)
+    if b %2 == 0:
+        return tmp * tmp % c
+    else:
+        return tmp * tmp % c * a % c
+print(cal(n,m,c))
