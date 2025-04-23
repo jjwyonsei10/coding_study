2851019fbdf2ec8d95179c1f16e99293ced9e075
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-23 12:13:51 +0900

    [Silver III] Title: 주유소, Time: 140 ms, Memory: 136728 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,13 +1,15 @@
 n = int(input())
 dist = list(map(int, input().split()))
 val = list(map(int, input().split()))
-
-init = dist[0] * val[0]
-
-for i in range(1, n):
-    if val[i] == min(val[:n-1]):
-        init+= val[i] * sum(dist[i:n-1])
-        break
-    else:
-        init+= val[i] * dist[i]
-print(init)
+stack = []
+res = 0
+for i in range(n-1):
+    if stack and val[stack[-1]] > val[i]:
+        v = stack.pop()
+        res += val[v] * sum(dist[v:i])
+        stack.append(i)
+    elif len(stack) == 0:
+        stack.append(i)
+if stack:
+    res+= val[stack[-1]] * sum(dist[stack[-1]:n-1])
+print(res)
