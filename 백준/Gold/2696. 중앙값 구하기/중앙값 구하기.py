c80ee85078ea4f20243c8011e5e030e16898ebc5
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-27 00:11:29 +0900

    [Gold II] Title: 중앙값 구하기, Time: 1904 ms, Memory: 114000 KB -BaekjoonHub

---
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,13 @@
+from sys import stdin
+input = lambda: stdin.readline().rstrip()
+T = int(input())
+for _ in range(T):
+    M = int(input())
+    nums = []
+    for _ in range(M // 10 + 1):
+        nums += list(map(int, input().split()))
+
+    print(M // 2 + 1)
+    for i in range(1, len(nums) + 1, 2):
+        print(sorted(nums[:i])[i // 2], end=' ')
+    print()
