30da49f5050938897a2b249458d5d85063cf9e73
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-28 14:45:29 +0900

    [Silver I] Title: 정렬, Time: 128 ms, Memory: 136368 KB -BaekjoonHub

---
+++ 백준/Silver/17074. 정렬/정렬.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+dp = []
+
+if arr[0] > arr[1]:
+    dp.append(1)
+else:
+    dp.append(0)
+
+for i in range(1, n-1):
+    if arr[i] <= arr[i+1]:
+        dp.append(0)
+    else:
+        dp.append(1)
+
+res = []
+for i in range(len(dp)):
+    if dp[i] == 1:
+        res.append(i)
+cnt = 0
+if len(res) == 1:
+    id = res.pop()
+    for k in range(id, id+2):
+        dp = [0] * (n-1)
+        tmp = arr[:k] + arr[k+1:]
+        check = True
+        for i in range(n-2):
+            if tmp[i] > tmp[i+1]:
+                check= False
+        if check:
+            cnt+=1
+    print(cnt)
+elif len(res) > 1:
+    print(0)
+else:
+    print(n)
