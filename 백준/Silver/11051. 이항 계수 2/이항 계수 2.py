8709f47bfa55556621249652c4b5dceacd2610f3
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-04-29 16:04:33 +0900

    [Silver II] Title: 이항 계수 2, Time: 104 ms, Memory: 116564 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -10,5 +10,5 @@ for i in range(1, n+1):
 
 for i in range(2, n+1):
     for j in range(2, i+1):
-        dp[i][j] = (dp[i-1][j-1] % 10007 + dp[i-1][j] % 10007) % 10007
+        dp[i][j] = (dp[i-1][j-1]+ dp[i-1][j]) % 10007
 print(dp[n][m])
