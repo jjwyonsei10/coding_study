bb2d5e597bde22528a092a7bd67d6ff68efb56f4
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-05-05 15:45:39 +0900

    [Gold III] Title: 합이 같은 구간, Time: 1692 ms, Memory: 113356 KB -BaekjoonHub

--- 백준/Gold/2694. 합이 같은 구간/합이 같은 구간.py
+++ 백준/Gold/2694. 합이 같은 구간/합이 같은 구간.py
@@ -12,13 +12,12 @@ for t in range(train):
         dp[i] = dp[i-1] + arr[i]
     total_sum = dp[-1]
 
-    # 조건에 맞는 값 찾기
     for ind in dp:
-        if total_sum % ind == 0:  # ind가 총합을 나눌 수 있는지 확인
+        if total_sum % ind == 0:
             cnt = 0
-            for value in dp:
-                if value % ind == 0:  # ind로 나눠지는 누적 합의 개수 세기
-                    cnt += 1
-            if cnt == total_sum // ind:  # 조건 만족 시 ind 출력
+            for val in dp:
+                if val%ind == 0:
+                    cnt+=1
+            if cnt == total_sum//ind:
                 print(ind)
                 break
