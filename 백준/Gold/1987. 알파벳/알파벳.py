e73f7c43f3e8b334e9ba8172a0e9e1cd9b55e7a6
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-05-20 10:04:36 +0900

    [Gold IV] Title: 알파벳, Time: 676 ms, Memory: 270180 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,18 +1,32 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-def dfs(x,y,cnt):
-    global max_
-    max_ = max(max_, cnt)
-    for nx, ny in ([x+1, y], [x-1, y], [x, y+1], [x, y-1]):
-        if 0<=nx<n and 0<=ny<m and arr[nx][ny] not in alpha:
-            alpha.add(arr[nx][ny])
-            dfs(nx,ny,cnt+1)
-            alpha.remove(arr[nx][ny])
-    
 
-n,m = map(int, input().split())
-arr  = [list(input().strip()) for _ in range(n)]
-max_ = 1
-alpha = set(arr[0][0])
-dfs(0,0,max_)
-print(max_)
+def bfs(x, y, visit, cnt):
+    global max_len
+    q = deque()
+    q.append((x, y, visit, cnt))
+
+    visited = set()
+    visited.add((x, y, visit))
+
+    while q:
+        x, y, visit, cnt = q.popleft()
+        max_len = max(max_len, cnt)
+
+        for nx, ny in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+            if 0 <= nx < n and 0 <= ny < m:
+                alpha = ord(arr[nx][ny]) - ord('A')
+                if not (visit & (1 << alpha)):
+                    new_visit = visit | (1 << alpha)
+                    if (nx, ny, new_visit) not in visited:
+                        visited.add((nx, ny, new_visit))
+                        q.append((nx, ny, new_visit, cnt + 1))
+
+n, m = map(int, input().split())
+arr = [list(input().strip()) for _ in range(n)]
+
+start = ord(arr[0][0]) - ord("A")
+max_len = 1
+bfs(0, 0, 1 << start, 1)
+print(max_len)
