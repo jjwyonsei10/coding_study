d669d7549402808552f3ecc92237ca5756f937a1
jjwyonsei10 <woojj10@yonsei.ac.kr>
2025-06-18 10:44:59 +0900

    [Silver I] Title: 경로 찾기, Time: 128 ms, Memory: 112288 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,13 @@
+from collections import deque
+n  = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visit = [0] * n
+def dfs(x):
+    for i in range(n):
+        if arr[x][i] and not visit[i]:
+            visit[i] = 1
+            dfs(i)
+for i in range(n):
+    dfs(i)
+    print(*visit)
+    visit = [0] * n
